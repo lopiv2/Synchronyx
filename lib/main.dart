13e9ba3b055ca15aa4fcf0dd2c9ea2f6c37aa489
Ivan Lopez <lopiv2@hotmail.com>
2023-07-26 13:20:41 +0200

    Fixed layout and Main Menu

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainGrid extends StatelessWidget {
+/*class MainGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -58,79 +58,144 @@ class MainGrid extends StatelessWidget {
       ],
     );
   }
-}
+}*/
 
-class LeftSide extends StatelessWidget {
+class MainGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 200,
-      child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-                color: Color.fromARGB(255, 2, 34, 14), // Color del borde
-                width: 0.2, // Ancho del borde
-              ),
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [sideBarColor, Color.fromARGB(255, 33, 109, 72), Color.fromARGB(255, 48, 87, 3)],
-                ),
-          ),
-          child: Column(
-            children: [
-              WindowTitleBarBox(
-                child: MoveWindow(),
-              ),
-              //Padding(padding: EdgeInsets.only(top: 10.0)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Container(
+        color: Colors.green,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                MoveWindow(),
+                /*WindowTitleBarBox(
+                  child: MoveWindow(),
+                ),*/
+                //Text("hola"),
+                //Text("hola"),
+                const WindowButtons(),
+              ],
+            ),
+            Expanded(
+              // Utiliza Expanded aquí para que el Column ocupe todo el espacio vertical disponible
+              child: Row(
                 children: [
                   Column(
-                    children: const [
-                      Icon(Icons.favorite, size: 20, color: Colors.red),
+                    children: [
+                      Container(
+                        color: Colors.red,
+                      )
                     ],
                   ),
-                  Column(
-                    children: const [
-                      Text('Synchronyx'),
-                    ],
-                  ),
-                  Column(
-                    children: const [
-                      Icon(Icons.menu, size: 20, color: Colors.blue),
-                    ],
-                  ),
+                  LeftSide(),
+                  RightSide(),
                 ],
               ),
-              const Padding(padding: EdgeInsets.only(top: 20.0)),
-              Row(
-                children: const <Widget>[
-                  SizedBox(width: 10), // give it width
-                  Flexible(
-                      child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.amber,
-                      border:
-                          OutlineInputBorder(), // Aquí establecemos el borde
-                      hintText: 'Search', // Texto de ayuda dentro del TextField
-                      // Otros estilos de la decoración (opcional)
-                      // labelStyle: TextStyle(color: Colors.red),
-                      // hintStyle: TextStyle(color: Colors.grey),
-                    ),
-                  )),
+            ),
+          ],
+        ));
+  }
+}
+
+/*WindowTitleBarBox(
+                  child: Row(
+                children: [
+                  Expanded(
+                    child: MoveWindow(),
+                  ),
+                  const WindowButtons()
+                ],
+              )),*/
+
+class LeftSide extends StatelessWidget {
+  const LeftSide({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: const Color.fromARGB(255, 2, 34, 14), // Color del borde
+          width: 0.2, // Ancho del borde
+        ),
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            sideBarColor,
+            const Color.fromARGB(255, 33, 109, 72),
+            const Color.fromARGB(255, 48, 87, 3)
+          ],
+        ),
+      ),
+      child: Column(
+        children: [
+          WindowTitleBarBox(
+            child: MoveWindow(),
+          ),
+          //Padding(padding: EdgeInsets.only(top: 10.0)),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Icon(Icons.favorite, size: 20, color: Colors.red),
                 ],
               ),
-              const Padding(padding: EdgeInsets.only(top: 20.0)),
-              DropdownWidget(),
+              Column(
+                children: [
+                  Text('Synchronyx'),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.menu, size: 20, color: Colors.blue),
+                ],
+              ),
             ],
-          )),
+          ),
+
+          const Padding(padding: EdgeInsets.only(top: 20.0)),
+          Container(
+            height: 30,
+            child: const Row(
+              children: <Widget>[
+                SizedBox(width: 10), // give it width
+                Flexible(
+                    child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10, horizontal: 6),
+                    filled: true,
+                    fillColor: Color.fromARGB(127, 11, 129, 46),
+                    border: OutlineInputBorder(), // Aquí establecemos el borde
+                    hintText: 'Search', // Texto de ayuda dentro del TextField
+                    // Otros estilos de la decoración (opcional)
+                    // labelStyle: TextStyle(color: Colors.red),
+                    // hintStyle: TextStyle(color: Colors.grey),
+                  ),
+                  style: TextStyle(fontSize: 14),
+                )),
+                SizedBox(width: 10), // give it width
+              ],
+            ),
+          ),
+
+          const Padding(padding: EdgeInsets.only(top: 20.0)),
+          const DropdownWidget(),
+        ],
+      ),
     );
   }
 }
 
 class DropdownWidget extends StatefulWidget {
+  const DropdownWidget({super.key});
+
   @override
   DropDownCategories createState() => DropDownCategories();
 }
@@ -147,57 +212,64 @@ class DropDownCategories extends State<DropdownWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.amber,
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(),
-      ),
-      child: DropdownButton(
-        alignment: Alignment.topCenter,
-        borderRadius: BorderRadius.circular(8),
-        dropdownColor: Colors.blueAccent,
-        value: currentItem,
-        onChanged: (String? newValue) {
-          setState(() {
-            currentItem = newValue ?? "";
-          });
-        },
-        items: items.map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            alignment: Alignment.center,
-            child: Text(value),
-          );
-        }).toList(),
+    return Padding(
+      padding: const EdgeInsets.only(
+          left: 10, right: 10), // Margen izquierdo deseado
+      child: Container(
+        height: 30,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(90, 28, 102, 50),
+          borderRadius: BorderRadius.circular(2),
+          border: Border.all(),
+        ),
+        child: DropdownButton(
+          underline: Container(),
+          style: TextStyle(
+              fontSize: 14, color: const Color.fromARGB(255, 36, 29, 29)),
+          isExpanded:
+              true, // Hace que el DropdownButton ocupe todo el ancho disponible
+          alignment: Alignment.topCenter,
+          borderRadius: BorderRadius.circular(8),
+          dropdownColor: const Color.fromARGB(255, 45, 114, 72),
+          value: currentItem,
+          onChanged: (String? newValue) {
+            setState(() {
+              currentItem = newValue ?? "";
+            });
+          },
+          items: items.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              alignment: Alignment.topLeft,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
 }
 
 class RightSide extends StatelessWidget {
+  const RightSide({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [backgroundStartColor, backgroundEndColor, Color.fromARGB(255, 48, 87, 3)],
-                ),
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                backgroundStartColor,
+                backgroundEndColor,
+                const Color.fromARGB(255, 48, 87, 3)
+              ],
+            ),
           ),
           child: Column(
             children: [
-              WindowTitleBarBox(
-                  child: Row(
-                children: [
-                  Expanded(
-                    child: MoveWindow(),
-                  ),
-                  WindowButtons()
-                ],
-              )),
               ArcadeBoxButtonWidget(),
             ],
           )),
@@ -206,6 +278,8 @@ class RightSide extends StatelessWidget {
 }
 
 class WindowButtons extends StatelessWidget {
+  const WindowButtons({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Row(children: [
